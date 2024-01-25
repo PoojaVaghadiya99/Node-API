ce7a81f1cc221a8d6274f6754db2686a29c0c4a6
cspoojavaghadiya <poojavaghadiya99@gmail.com>
2024-01-25 15:16:05 +0530

    Added Search, Short, Pagination Using DB

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,49 @@
-const getAllProducts = async(req,res) => {
-    res.status(200).json({msg: "Get All Products"})
-}
+const product = require("../model/product");
 
-const getAllProductsTesting = async(req,res) => {
-    res.status(200).json({msg: "Get All Products Testing"})
-}
+const getAllProducts = async (req, res) => {
+  const { company, name, featured, sort, select } = req.query;
+  const queryObject = {};
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (featured) {
+    queryObject.featured = featured;
+  }
+
+  let apiData = product.find(queryObject);
+
+  if (sort) {
+    let shortFix = sort.split(",").join(" ");
+    apiData = apiData.sort(shortFix);
+  }
+
+  if (select) {
+    let selectFix = select.replace(",", " ");
+    apiData = apiData.select(selectFix);
+  }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+
+  let skip = (page - 1) * limit;
+  apiData = apiData.skip(skip).limit(limit);
+
+  const myData = await apiData;
+  res.status(200).json({ myData, nbHits:myData.length });
+};
+
+const getAllProductsTesting = async (req, res) => {
+  const myData = await product.find(req.query).select("name company");
+  res.status(200).json({ myData });
+};
 
 module.exports = {
-    getAllProducts,
-    getAllProductsTesting
-}
+  getAllProducts,
+  getAllProductsTesting,
+};
---
+++ model/product.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: [true, "Price Must be Provided"] },
+  featured: { type: Boolean, default: false },
+  rating: {
+    type: Number,
+    default: 4.9,
+    required: [true, "Rating Must be Provided"],
+  },
+  creditedAt:{type:Date,default:Date.now()},
+  company: {
+    type: String,
+    enum: {
+      values: ["apple", "samsung", "dell", "mi"],
+      message: `{VALUE} is not supported`,
+    },
+  },
+});
+
+module.exports = mongoose.model("Product",productSchema)
